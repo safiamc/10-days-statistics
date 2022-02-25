62b61c0e3f38067e98dbf7c4e2c9337cec4203bb
Safia Chettih <31898239+safiamc@users.noreply.github.com>
2022-02-25 14:47:42 -0600

    Create 0weighted_mean.py

---
+++ 0weighted_mean.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'weightedMean' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY X
+#  2. INTEGER_ARRAY W
+#
+
+def weightedMean(X, W):
+    prod_sum =0
+    weight_sum =0
+    for i in range(len(W)):
+        weight_sum += W[i]
+        prod_sum += X[i] * W[i]
+    print(round(prod_sum/weight_sum, 1))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    vals = list(map(int, input().rstrip().split()))
+
+    weights = list(map(int, input().rstrip().split()))
+
+    weightedMean(vals, weights)
